4fcb48882803fc6a3a32938bbaff9e3618ee59c4
teresa-tanzi <teresa.tanzi@studenti.unimi.it>
2021-11-08 15:19:07 +0100

    Quartet fix

--- Sandwich.c
+++ Sandwich.c
@@ -50,20 +50,6 @@ static void generateRelatedKeys(u8 Ka[]) {
     }
 }
 
-/*-------------------------------------------------------------------------------------------
- * 1. Data Collection Phase:
- *-------------------------------------------------------------------------------------------*/
-
- /*-------------------------------------------------------------------------------------------
- *	(a) Choose a structure of 2^24 ciphertexts of the form C_a = (X_a, A), where
- *		A is ﬁxed and X a assumes 2^24 arbitrary diﬀerent values. Ask for the
- * 		decryption of all the ciphertexts under the key K_a and denote the plain-
- * 		text corresponding to C_a by P_a . For each P_a , ask for the encryption of
- * 		P_b = P_a ⊕ (0_x, 0010 0000_x) under the key K_b and denote the resulting
- * 		ciphertext by C_b . Store the pairs (C_a, C_b ) in a hash table indexed by the
- * 		32-bit value C_b^R (i.e., the right half of C_b ).
- *-------------------------------------------------------------------------------------------*/
-
 int main(void) {
 	time_t t;
 
@@ -81,6 +67,10 @@ int main(void) {
     printHex("Kd", Kd, 16);
 
     /*-------------------------------------------------------------------------------------------
+	 * 1. Data Collection Phase:
+	 *-------------------------------------------------------------------------------------------*/
+
+    /*-------------------------------------------------------------------------------------------
 	 *	(a) Choose a structure of 2^24 ciphertexts of the form C_a = (X_a, A), where
 	 *		A is ﬁxed and X a assumes 2^24 arbitrary diﬀerent values. 
 	 *-------------------------------------------------------------------------------------------*/
@@ -89,11 +79,16 @@ int main(void) {
 	srand((unsigned) time(&t));
 
 	u8 Ca[8];
-	u8 A = 0xff;
-	for (int i = 0; i < 7; i++) {
-		Ca[i] = rand() % 255;		// 255_10 = ff_16 = 11111111_2
+    u8 A[4] = {
+        0xff, 0xff, 0xff, 0xff,
+    };
+    for (int i = 0; i < 4; i++) {
+        //Ca[i] = rand() % 255;     // 255_10 = ff_16 = 11111111_2
+        Ca[i] = 0x11;
     }
-    Ca[7] = A;
+    for (int i = 0; i < 4; i++) {
+        Ca[4+i] = A[i];
+    }
 
     printHex("Ca", Ca, 8);
 
@@ -113,10 +108,12 @@ int main(void) {
 	 *-------------------------------------------------------------------------------------------*/
 
     u8 Pb[8];
-    for (int i = 0; i < 6; i++) {
-    	Pb[i] = Ca[i];
+    for (int i = 0; i < 8; i++) {
+        if (i != 5)
+    	    Pb[i] = Ca[i];
+        else 
+            Pb[i] = Ca[i] ^ 0x10;
     }
-    Pb[7] = Ca[7] ^ 0x20;
 
     printHex("Pb", Pb, 8);
 
@@ -125,12 +122,53 @@ int main(void) {
 
     printHex("Cb", Pb, 8);
 
+    /*-------------------------------------------------------------------------------------------
+	 *	(b) Choose a structure of 2^24 ciphertexts of the form C_c = (Y_c , A xor 0010 0000_x ),
+	 *		where A is the same constant as before, and Y_c assumes 2^24 arbitrary dif-
+	 *		ferent values. 
+	 *-------------------------------------------------------------------------------------------*/
 
-    
+    u8 Cc[8];
+    for (int i = 0; i < 4; i++) {
+        //Ca[i] = rand() % 255;     // 255_10 = ff_16 = 11111111_2
+        Cc[i] = 0xaa;
+    }
+    for (int i = 0; i < 4; i++) {
+        if (i != 1)
+            Cc[4+i] = A[i];
+        else
+            Cc  [4+i] = A[i] ^ 0x10;
+    }
 
-    /*
-    u8 text[8]  = {
-        0xFE, 0xDC, 0xBA, 0x09, 0x87, 0x65, 0x43, 0x21,
-    };
-    */
+    printHex("Cc", Cc, 8);    
+
+    /*-------------------------------------------------------------------------------------------
+	 *		Ask for the decryption of the ciphertexts under the key K_c
+	 * 		and denote the plaintext corresponding to C_c by P_c. 
+	 *-------------------------------------------------------------------------------------------*/
+
+    KeySchedule(Kc);
+    KasumiDecipher(Cc);
+
+    printHex("Pc", Cc, 8);
+
+    /*-------------------------------------------------------------------------------------------
+	 *		For each P_c , ask for the encryption of P_d = P_c xor (0_x , 0010 0000_x)
+	 *		under the key K_d and denote the resulting ciphertext by C_d .
+	 *-------------------------------------------------------------------------------------------*/
+
+    u8 Pd[8];
+    for (int i = 0; i < 8; i++) {
+        if (i != 5)
+            Pd[i] = Cc[i];
+        else
+            Pd[i] = Cc[i] ^ 0x10;
+    }
+
+    printHex("Pd", Pd, 8);
+
+    KeySchedule(Kd);
+    Kasumi(Pd);
+
+    printHex("Cd", Pd, 8);
 }
