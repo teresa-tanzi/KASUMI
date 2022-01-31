8bb4db98059ac232f2eeeabde135b6c581ccd13c
teresa-tanzi <teresa.tanzi@studenti.unimi.it>
2022-01-31 01:49:09 +0100

    Wrong previous update

--- Rectangle.c
+++ Rectangle.c
@@ -92,7 +92,7 @@ struct hashEntry {
 
 struct hashEntry *hashTable = NULL;
 
-void addEntry(u8 index[], u8 Pa[], u8 Pb[]) {dd
+void addEntry(u8 index[], u8 Pa[], u8 Pb[]) {
 	struct hashEntry *h;
 	//struct hashValue *v;
 
