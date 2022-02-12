b5640632df06ad2b3ade97f33590f64b6ad09634
teresa-tanzi <teresa.tanzi@studenti.unimi.it>
2022-02-12 18:41:11 +0100

    Added the search for all the possible values of KO81 and KI81

--- Sandwich.c
+++ Sandwich.c
@@ -290,6 +290,32 @@ u16 rightRotate(u16 n, unsigned int d) {
     return (n >> d) | (n << (16 - d));
 }
 
+/*------------------------------------- LOOKUP TABLE ---------------------------------------*/
+
+/* 	- 0, 1 	: the guessed bit for KL_8,2 is 0/1 			*
+ * 	- 2 	: the guessed bit can be both 0 and 1 			*
+ *	- 3 	: there is not a possible guessing for the key 	*/
+
+/*
+static short OR[] = 
+{
+  2, 3, 1, 0,
+  3, 3, 3, 3,
+  1, 3, 1, 3,
+  0, 3, 3, 0
+};
+*/
+
+/*
+static short AND[] = 
+{
+  2, 3, 0, 1,
+  3, 3, 3, 3,
+  0, 3, 0, 3,
+  1, 3, 3, 1
+};
+*/
+
 /*--------------------------------------- SANDWICH -----------------------------------------*/
 
 int main(void) {
@@ -666,37 +692,108 @@ int main(void) {
 
 		printf("Guessing the keys KO81 and KO82...\n");
 
-		printf("KO81:\t%04x\n", KO81);
-		printf("KI81:\t%04x\n", KI81);
+		z = 0;	// Initializing the progress bar
 
-		printf("Ca^LL:\t%04x\n", (u16)(Ca[0]<<8)+(Ca[1]));	// Ca^LL
-		printf("Ca^LR:\t%04x\n", (u16)(Ca[2]<<8)+(Ca[3]));	// Ca^LR
-		printf("Ca^RL:\t%04x\n", (u16)(Ca[4]<<8)+(Ca[5]));	// Ca^RL
-		printf("Ca^RR:\t%04x\n", (u16)(Ca[6]<<8)+(Ca[7]));	// Ca^RR
+		for (int i = 0; i <= 0xffff; i++) {
+			for (int j = 0; j <= 0xffff; j++) {
+				//printf("KO81:\t%04x\n", KO81);
+				//printf("KI81:\t%04x\n", KI81);
 
-		printf("Cc^LL:\t%04x\n", (u16)(Cc[0]<<8)+(Cc[1]));	// Cc^LL
-		printf("Cc^LR:\t%04x\n", (u16)(Cc[2]<<8)+(Cc[3]));	// Cc^LR
-		printf("Cc^RL:\t%04x\n", (u16)(Cc[4]<<8)+(Cc[5]));	// Cc^RL
-		printf("Cc^RR:\t%04x\n", (u16)(Cc[6]<<8)+(Cc[7]));	// Cc^RR
+				/*
+				printf("Ca^LL:\t%04x\n", (u16)(Ca[0]<<8)+(Ca[1]));	// Ca^LL
+				printf("Ca^LR:\t%04x\n", (u16)(Ca[2]<<8)+(Ca[3]));	// Ca^LR
+				printf("Ca^RL:\t%04x\n", (u16)(Ca[4]<<8)+(Ca[5]));	// Ca^RL
+				printf("Ca^RR:\t%04x\n", (u16)(Ca[6]<<8)+(Ca[7]));	// Ca^RR
 
-		Xac = ((u16)(Ca[2]<<8)+(Ca[3])) ^ ((u16)(Cc[2]<<8)+(Cc[3]));	// Ca^LR ^ Cc^LR
-		Xbd = ((u16)(Cb[2]<<8)+(Cb[3])) ^ ((u16)(Cd[2]<<8)+(Cd[3]));	// Cb^LR ^ Cd^LR
-		printf("Xac:\t%04x\n", Xac);
-		printf("Xbd:\t%04x\n", Xbd);
+				printf("Cc^LL:\t%04x\n", (u16)(Cc[0]<<8)+(Cc[1]));	// Cc^LL
+				printf("Cc^LR:\t%04x\n", (u16)(Cc[2]<<8)+(Cc[3]));	// Cc^LR
+				printf("Cc^RL:\t%04x\n", (u16)(Cc[4]<<8)+(Cc[5]));	// Cc^RL
+				printf("Cc^RR:\t%04x\n", (u16)(Cc[6]<<8)+(Cc[7]));	// Cc^RR
+				*/
 
-		X1aR = FI(((u16)(Ca[4]<<8)+(Ca[5])) ^ KO81, KI81) ^ ((u16)(Ca[6]<<8)+(Ca[7]));	// FI81( Ca^RL ^ KO81, KI81 ) ^ Ca^RR
-		X1cR = FI(((u16)(Cc[4]<<8)+(Cc[5])) ^ KO81, KI81) ^ ((u16)(Cc[6]<<8)+(Cc[7]));	// FI81( Cc^RL ^ KO81, KI81 ) ^ Cc^RR
-		X1bR = FI(((u16)(Cb[4]<<8)+(Cb[5])) ^ KO81, KI81) ^ ((u16)(Cb[6]<<8)+(Cb[7]));	// FI81( Cb^RL ^ KO81, KI81 ) ^ Cb^RR
-		X1dR = FI(((u16)(Cd[4]<<8)+(Cd[5])) ^ KO81, KI81) ^ ((u16)(Cd[6]<<8)+(Cd[7]));	// FI81( Cd^RL ^ KO81, KI81 ) ^ Cd^RR
-		printf("X1aR:\t%04x\n", X1aR);
-		printf("X1cR:\t%04x\n", X1cR);
-		printf("X1bR:\t%04x\n", X1bR);
-		printf("X1dR:\t%04x\n", X1dR);
+				Xac = ((u16)(Ca[2]<<8)+(Ca[3])) ^ ((u16)(Cc[2]<<8)+(Cc[3]));	// Ca^LR ^ Cc^LR
+				Xbd = ((u16)(Cb[2]<<8)+(Cb[3])) ^ ((u16)(Cd[2]<<8)+(Cd[3]));	// Cb^LR ^ Cd^LR
+				/*
+				printf("Xac:\t%04x\n", Xac);
+				printf("Xbd:\t%04x\n", Xbd);
+				*/
 
-		Yac = rightRotate(((u16)(Ca[0]<<8)+(Ca[1])) ^ ((u16)(Cc[0]<<8)+(Cc[1])) ^ X1aR ^ X1cR, 1);	// ( Ca^LL ^ Cc^LL ^ X1a^R ^ X1c^R ) >>> 1
-		Ybd = rightRotate(((u16)(Cb[0]<<8)+(Cd[1])) ^ ((u16)(Cb[0]<<8)+(Cd[1])) ^ X1bR ^ X1dR, 1);	// ( Cb^LL ^ Cd^LL ^ X1b^R ^ X1d^R ) >>> 1
-		printf("Yac:\t%04x\n", Yac);
-		printf("Ybd:\t%04x\n", Ybd);
+				X1aR = FI(((u16)(Ca[4]<<8)+(Ca[5])) ^ KO81, KI81) ^ ((u16)(Ca[6]<<8)+(Ca[7]));	// FI81( Ca^RL ^ KO81, KI81 ) ^ Ca^RR
+				X1cR = FI(((u16)(Cc[4]<<8)+(Cc[5])) ^ KO81, KI81) ^ ((u16)(Cc[6]<<8)+(Cc[7]));	// FI81( Cc^RL ^ KO81, KI81 ) ^ Cc^RR
+				X1bR = FI(((u16)(Cb[4]<<8)+(Cb[5])) ^ KO81, KI81) ^ ((u16)(Cb[6]<<8)+(Cb[7]));	// FI81( Cb^RL ^ KO81, KI81 ) ^ Cb^RR
+				X1dR = FI(((u16)(Cd[4]<<8)+(Cd[5])) ^ KO81, KI81) ^ ((u16)(Cd[6]<<8)+(Cd[7]));	// FI81( Cd^RL ^ KO81, KI81 ) ^ Cd^RR
+				/*
+				printf("X1aR:\t%04x\n", X1aR);
+				printf("X1cR:\t%04x\n", X1cR);
+				printf("X1bR:\t%04x\n", X1bR);
+				printf("X1dR:\t%04x\n", X1dR);
+				*/
+
+				Yac = rightRotate(((u16)(Ca[0]<<8)+(Ca[1])) ^ ((u16)(Cc[0]<<8)+(Cc[1])) ^ X1aR ^ X1cR, 1);	// ( Ca^LL ^ Cc^LL ^ X1a^R ^ X1c^R ) >>> 1
+				Ybd = rightRotate(((u16)(Cb[0]<<8)+(Cd[1])) ^ ((u16)(Cb[0]<<8)+(Cd[1])) ^ X1bR ^ X1dR, 1);	// ( Cb^LL ^ Cd^LL ^ X1b^R ^ X1d^R ) >>> 1
+				/*
+				printf("Yac:\t%04x\n", Yac);
+				printf("Ybd:\t%04x\n", Ybd);
+				*/
+
+				//printf("OR:\t");
+				while (Xac) {
+					int i = 0;
+					int j = 0;
+					//int b;
+
+				    if (Xac & 1) i += 2;	// Current bit is set to 1
+				    if (Yac & 1) i += 1;
+				    if (Xbd & 1) j += 2;
+				    if (Ybd & 1) j += 1;
+
+				    //b = OR[4*i + j];
+
+				    /*
+				    if (b == 3) {
+				    	printf("Inconsistency found.\n");
+				    	break;
+				    } else if (b == 2) {
+				    	printf("Duplicate the key.\n");
+				    } else {
+				    	printf("%d", b);
+				    }
+				    */
+
+				    //printf("%d", OR[4*i + j]);
+				    
+				    Xac >>= 1;
+					Yac >>= 1;
+					Xbd >>= 1;
+					Ybd >>= 1;
+				}
+				//printf("\n");
+
+				//if (KI81 < 0xffff) KI81++;
+				KI81++;
+
+				if ((u32)((KO81<<16)+(KI81)) > z * (pow(2,32)/100.0)) {
+					printProgress(z/100.0);
+					z++;
+				} else if ((u32)((KO81<<16)+(KI81)) == pow(2,32) - 1) {
+					printProgress(1);
+				}
+
+				//printf("%08x\n", (u32)((KO81<<16)+(KI81)));
+
+			}
+
+			//if (KO81 < 0xffff) {
+				KO81++;
+				KI81 = 0x0000;
+			//}
+		}
+		printf("\n");
+		/*
+		printf("%x\n", (u32)((KO81<<16)+(KI81)));
+		printf("%x\n", (int)pow(2,32));
+		printf("%f\n", pow(2,32));
+		*/
 
 		cont++;
 		break;
